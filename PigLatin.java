0b077bad878fb5c7aafc644269642836d0c744f0
turtlelazy <ishraqmahid@gmail.com>
2021-01-04 14:09:07 -0500

    pigLatinSimple method created

--- PigLatin.java
+++ PigLatin.java
@@ -1,5 +1,20 @@
 public class PigLatin {
     public static void main(String[] args){
-        
+
+    }
+    
+    public static String pigLatinSimple(String s) {
+        String sCopy = s;
+        char[] vowels = {'a','e','i','o','u'};
+        for(int i = 0;i<vowels.length;i++) {
+            if(s.charAt(0) == vowels[i]) {
+                s += "hay";
+                return sCopy;
+            }
+        }
+
+        sCopy = sCopy.substring(1) + sCopy.charAt(0) + "ay";
+        return sCopy;
+
     }
 }
