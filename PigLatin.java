67ed6a9c279328db4d0fc763c3836cc11bfff96a
turtlelazy <ishraqmahid@gmail.com>
2021-01-05 17:07:37 -0500

    pigLatinSimple tested and pigLatin helper created

--- PigLatin.java
+++ PigLatin.java
@@ -1,6 +1,10 @@
 public class PigLatin {
     public static void main(String[] args){
-
+        String[] words = {"mock", "pie", "david","aaron"};
+        String[] wordsLatin = { "ockmay","iepay","avidday", "aaronhay"};
+        for(int i = 0; i < words.length;i++){
+            System.out.println(pigLatinSimple(words[i]).equals(wordsLatin[i]));
+        }
     }
     
     public static String pigLatinSimple(String s) {
@@ -8,7 +12,7 @@ public class PigLatin {
         char[] vowels = {'a','e','i','o','u'};
         for(int i = 0;i<vowels.length;i++) {
             if(s.charAt(0) == vowels[i]) {
-                s += "hay";
+                sCopy += "hay";
                 return sCopy;
             }
         }
@@ -16,5 +20,23 @@ public class PigLatin {
         sCopy = sCopy.substring(1) + sCopy.charAt(0) + "ay";
         return sCopy;
 
+    }
+
+    public static String pigLatin(String s){
+        String sCopy = s;
+        
+    }
+
+    public static boolean diagraphCheck(String s){
+        String[] diagraphs = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", 
+        "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", 
+        "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+
+        for(int i = 0; i < diagraphs.length;i++){
+            if(s.substring(0,2).equals(diagraphs[i])){
+                return true;
+            }
+        }
+        return false;
     }
 }
